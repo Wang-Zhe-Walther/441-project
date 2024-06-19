382fcd2954973aba30ccd5c08ad2c338689be8c2
Wang-Zhe-Walther <164817161+Wang-Zhe-Walther@users.noreply.github.com>
2024-06-19 21:33:18 +0800

    Add files via upload

---
+++ login.js
@@ -0,0 +1,18 @@
+//StUdEnt Name: Walther Wang Zhe
+document.getElementById('loginForm').addEventListener('submit', function(event) {  
+  event.preventDefault(); // Prevent form submission  
+
+  let username = document.getElementById('username').value;  
+  let password = document.getElementById('password').value;  
+
+  let storedUsername = localStorage.getItem("username");
+  let storedPassword = localStorage.getItem("password");
+
+  if (username === storedUsername && password === storedPassword) {  
+      // Login successful, redirect to cart page  
+      window.location.href = 'home.html'; // Redirect to cart page
+  } else {  
+      // Login failed, show an alert  
+      alert('Invalid username or password. Please try again.');  
+  }  
+});
